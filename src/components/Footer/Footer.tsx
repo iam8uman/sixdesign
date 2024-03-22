178c6ac9ea5c8c52bd4fd050da7a6bebdc3a51b1
iam8uman <me8848you@gmail.com>
2024-03-22 12:45:23 +0545

    Footer section full update along with email

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -18,7 +18,7 @@ const Footerr = () => {
         <div className="px-4 mx-auto sm:px-6 lg:px-8 max-w-7xl">
           <div className="flex flex-wrap items-start md:justify-between gap-y-12 gap-x-24 md:gap-x-12">
             <div className="w-full lg:w-64 xl:w-full xl:max-w-xs">
-              <Link href="#" title="" className="flex">
+              <Link href="/" title="" className="flex">
                 <div className="text-2xl text-black sm:text-3xl lg:text-4xl flex gap-1 font-bold lg:justify-start align-middle mb-3 justify-center ">
                   <SixdesignLogo size="sm" />
                   <div className="text-black">SixDesign</div>
@@ -26,7 +26,9 @@ const Footerr = () => {
               </Link>
 
               <p className="my-4 text-base font-normal text-gray-500">
-              We specialize in creating high-quality websites, captivating landing pages, and visually appealing designs tailored for realtors, brokerages, and builders.
+                We specialize in creating high-quality websites, captivating
+                landing pages, and visually appealing designs tailored for
+                realtors, brokerages, and builders.
               </p>
             </div>
 
@@ -38,7 +40,7 @@ const Footerr = () => {
               <ul className="mt-8 space-y-5">
                 <li>
                   <Link
-                    href="#"
+                    href="/about"
                     title=""
                     className="inline-flex text-base font-medium text-gray-900 transition-all duration-200 hover:translate-x-1 hover:text-blue-600"
                   >
@@ -49,34 +51,34 @@ const Footerr = () => {
 
                 <li>
                   <Link
-                    href="#"
+                    href="/services"
                     title=""
                     className="inline-flex text-base font-medium text-gray-900 transition-all duration-200 hover:translate-x-1 hover:text-blue-600"
                   >
                     {" "}
-                    Features{" "}
+                    Services
                   </Link>
                 </li>
 
                 <li>
                   <Link
-                    href="#"
+                    href="/blog"
                     title=""
                     className="inline-flex text-base font-medium text-gray-900 transition-all duration-200 hover:translate-x-1 hover:text-blue-600"
                   >
                     {" "}
-                    Works{" "}
+                    Blogs
                   </Link>
                 </li>
 
                 <li>
                   <Link
-                    href="#"
+                    href="/contact"
                     title=""
                     className="inline-flex text-base font-medium text-gray-900 transition-all duration-200 hover:translate-x-1 hover:text-blue-600"
                   >
                     {" "}
-                    Career{" "}
+                    Contact
                   </Link>
                 </li>
               </ul>
@@ -90,7 +92,7 @@ const Footerr = () => {
               <ul className="mt-8 space-y-5">
                 <li>
                   <Link
-                    href="#"
+                    href="/faqs"
                     title=""
                     className="inline-flex text-base font-medium text-gray-900 transition-all duration-200 hover:translate-x-1 hover:text-blue-600"
                   >
@@ -101,12 +103,12 @@ const Footerr = () => {
 
                 <li>
                   <Link
-                    href="#"
+                    href="/faqs"
                     title=""
                     className="inline-flex text-base font-medium text-gray-900 transition-all duration-200 hover:translate-x-1 hover:text-blue-600"
                   >
                     {" "}
-                    Delivery Details{" "}
+                    FAQS
                   </Link>
                 </li>
 
@@ -135,12 +137,69 @@ const Footerr = () => {
             </div>
 
             <div className="w-full md:max-w-xs">
-            <Link href="#" title="" className="flex">
-                <div className="text-2xl text-black sm:text-3xl lg:text-4xl flex gap-1 font-bold lg:justify-start align-middle mb-3 justify-center ">
+              <Link href="#" title="" className="flex">
+                <div className="text-2xl text-black sm:text-3xl lg:text-4xl flex gap-1 px-4 font-bold lg:justify-start align-middle mb-2 justify-center ">
                   <SixdesignLogo size="sm" />
-                  <div className="text-black">SixDesign</div>
+                  <div className="text-black mx-1">SixDesign</div>
                 </div>
               </Link>
+              <div className="listing">
+                <section className="py-6 bg-white sm:py-6 lg:py-10">
+                  <div className="px-4 mx-auto sm:px-4 lg:px-6 max-w-7xl">
+                    <div>
+                      <div className="space-y-5">
+                        <div className="flex items-start">
+                          <svg
+                            className="flex-shrink-0 text-red-500 w-7 h-7"
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke="currentColor"
+                          >
+                            <path
+                              stroke-linecap="round"
+                              stroke-linejoin="round"
+                              stroke-width="1.5"
+                              d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z"
+                            />
+                            <path
+                              stroke-linecap="round"
+                              stroke-linejoin="round"
+                              stroke-width="1.5"
+                              d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"
+                            />
+                          </svg>
+                          <span className="block ml-3 text-base font-medium text-gray-900">
+                            {" "}
+                            4517 Washington Ave. Manchester, Kentucky 39495{" "}
+                          </span>
+                        </div>
+
+                        <div className="flex items-start">
+                          <svg
+                            className="flex-shrink-0 text-red-500 w-7 h-7"
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke="currentColor"
+                          >
+                            <path
+                              stroke-linecap="round"
+                              stroke-linejoin="round"
+                              stroke-width="1.5"
+                              d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
+                            />
+                          </svg>
+                          <span className="block ml-3 text-base font-medium text-gray-900">
+                            {" "}
+                            team@sixdesign.ca
+                          </span>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </section>
+              </div>
             </div>
           </div>
 
