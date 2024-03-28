4a77f5f5a96a7fa60da2a74dc5137b7ac2ab6607
iam8uman <me8848you@gmail.com>
2024-03-28 22:58:22 +0545

    What we can page completed

--- src/components/Services/Whatwecan.tsx
+++ src/components/Services/Whatwecan.tsx
@@ -1,3 +1,5 @@
+import { CheckCircledIcon } from "@radix-ui/react-icons";
+import { CircleCheck } from "lucide-react";
 import React from "react";
 
 const Whatwecan = () => {
@@ -7,9 +9,12 @@ const Whatwecan = () => {
         <div className="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
           <div className="max-w-4xl mx-auto text-center">
             <h2 className="text-3xl font-bold leading-tight text-black sm:text-4xl lg:text-5xl">
-            What We Can Do For You            </h2>
+              What We Can Do For You{" "}
+            </h2>
             <p className="max-w-4xl mx-auto mt-4 text-xl leading-relaxed text-gray-600">
-            Elevate your online presence with our comprehensive website redesign services. We provide a seamless fusion of innovative design and robust development to meet all your website needs.
+              Elevate your online presence with our comprehensive website
+              redesign services. We provide a seamless fusion of innovative
+              design and robust development to meet all your website needs.
             </p>
           </div>
 
@@ -31,22 +36,71 @@ const Whatwecan = () => {
               </div>
 
               <div className="px-4 py-5 lg:p-8">
-                <p className="text-lg font-medium text-gray-500">1 Week</p>
-                <p className="mt-4 text-6xl font-bold text-black">$4</p>
-                <ul className="flex flex-col mt-8 space-y-1">
-                  <li className="text-base font-medium text-gray-900 lg:text-xl">
+                <p className="text-2xl font-bold  text-black">Web Design</p>
+                <div className="flex flex-col justify-start list list-disc mt-8 space-y-1 gap-y-4">
+                  <div className="text-base font-medium  text-gray-900 lg:text-xl flex gap-3 justify-center">
+                    <CircleCheck
+                      size={20}
+                      color="#000000"
+                      strokeWidth={3}
+                      absoluteStrokeWidth
+                      className="mt-1"
+                    />{" "}
                     Less than $1 a day
-                  </li>
-                  <li className="text-base font-medium text-gray-600 lg:text-xl">
-                    Billed every week
-                  </li>
-                  <li className="text-base font-medium text-gray-600 lg:text-xl">
-                    Cancel anytime
-                  </li>
-                </ul>
+                  </div>
+                  <div className="text-base font-medium  text-gray-900 lg:text-xl flex gap-3 justify-center">
+                    <CircleCheck
+                      size={20}
+                      color="#000000"
+                      strokeWidth={3}
+                      absoluteStrokeWidth
+                      className="mt-1"
+                    />{" "}
+                    Less than $1 a day
+                  </div>
+                  <div className="text-base font-medium  text-gray-900 lg:text-xl flex gap-3 justify-center">
+                    <CircleCheck
+                      size={20}
+                      color="#000000"
+                      strokeWidth={3}
+                      absoluteStrokeWidth
+                      className="mt-1"
+                    />{" "}
+                    Less than $1 a day
+                  </div>
+                  <div className="text-base font-medium  text-gray-900 lg:text-xl flex gap-3 justify-center">
+                    <CircleCheck
+                      size={20}
+                      color="#000000"
+                      strokeWidth={3}
+                      absoluteStrokeWidth
+                      className="mt-1"
+                    />{" "}
+                    Less than $1 a day
+                  </div>
+                  <div className="text-base font-medium  text-gray-900 lg:text-xl flex gap-3 justify-center">
+                    <CircleCheck
+                      size={20}
+                      color="#000000"
+                      strokeWidth={3}
+                      absoluteStrokeWidth
+                      className="mt-1"
+                    />{" "}
+                    Less than $1 a day
+                  </div>
+                  <div className="text-base font-medium  text-gray-900 lg:text-xl flex gap-3 justify-center">
+                    <CircleCheck
+                      size={20}
+                      color="#000000"
+                      strokeWidth={3}
+                      absoluteStrokeWidth
+                      className="mt-1"
+                    />{" "}
+                    Less than $1 a day
+                  </div>
+                </div>
               </div>
             </div>
-
             <div className="relative overflow-hidden bg-transparent border-2 border-gray-100 rounded-md">
               <div className="absolute hidden top-3 right-3">
                 <svg
@@ -64,24 +118,75 @@ const Whatwecan = () => {
               </div>
 
               <div className="px-4 py-5 lg:p-8">
-                <p className="text-lg font-medium text-gray-500">1 Month</p>
-                <p className="mt-4 text-6xl font-bold text-black">$19</p>
-                <ul className="flex flex-col mt-8 space-y-1">
-                  <li className="text-base font-medium text-gray-900 lg:text-xl">
-                    Less than $5 a week
-                  </li>
-                  <li className="text-base font-medium text-gray-600 lg:text-xl">
-                    Billed every month
-                  </li>
-                  <li className="text-base font-medium text-gray-600 lg:text-xl">
-                    Cancel anytime
-                  </li>
-                </ul>
+                <p className="text-2xl font-bold  text-black">
+                  CMS Integration
+                </p>
+                <div className="flex flex-col justify-start list list-disc mt-8 space-y-1 gap-y-4">
+                  <div className="text-base font-medium  text-gray-900 lg:text-xl flex gap-3 justify-center">
+                    <CircleCheck
+                      size={20}
+                      color="#000000"
+                      strokeWidth={3}
+                      absoluteStrokeWidth
+                      className="mt-1"
+                    />{" "}
+                    Less than $1 a day
+                  </div>
+                  <div className="text-base font-medium  text-gray-900 lg:text-xl flex gap-3 justify-center">
+                    <CircleCheck
+                      size={20}
+                      color="#000000"
+                      strokeWidth={3}
+                      absoluteStrokeWidth
+                      className="mt-1"
+                    />{" "}
+                    Less than $1 a day
+                  </div>
+                  <div className="text-base font-medium  text-gray-900 lg:text-xl flex gap-3 justify-center">
+                    <CircleCheck
+                      size={20}
+                      color="#000000"
+                      strokeWidth={3}
+                      absoluteStrokeWidth
+                      className="mt-1"
+                    />{" "}
+                    Less than $1 a day
+                  </div>
+                  <div className="text-base font-medium  text-gray-900 lg:text-xl flex gap-3 justify-center">
+                    <CircleCheck
+                      size={20}
+                      color="#000000"
+                      strokeWidth={3}
+                      absoluteStrokeWidth
+                      className="mt-1"
+                    />{" "}
+                    Less than $1 a day
+                  </div>
+                  <div className="text-base font-medium  text-gray-900 lg:text-xl flex gap-3 justify-center">
+                    <CircleCheck
+                      size={20}
+                      color="#000000"
+                      strokeWidth={3}
+                      absoluteStrokeWidth
+                      className="mt-1"
+                    />{" "}
+                    Less than $1 a day
+                  </div>
+                  <div className="text-base font-medium  text-gray-900 lg:text-xl flex gap-3 justify-center">
+                    <CircleCheck
+                      size={20}
+                      color="#000000"
+                      strokeWidth={3}
+                      absoluteStrokeWidth
+                      className="mt-1"
+                    />{" "}
+                    Less than $1 a day
+                  </div>
+                </div>
               </div>
             </div>
-
-            <div className="relative overflow-hidden bg-white border-2 border-gray-900 rounded-md shadow">
-              <div className="absolute top-3 right-3">
+            <div className="relative overflow-hidden bg-transparent border-2 border-gray-100 rounded-md">
+              <div className="absolute hidden top-3 right-3">
                 <svg
                   className="w-6 h-6 text-orange-500"
                   xmlns="http://www.w3.org/2000/svg"
@@ -97,36 +202,71 @@ const Whatwecan = () => {
               </div>
 
               <div className="px-4 py-5 lg:p-8">
-                <p className="text-lg font-medium text-gray-500">1 Year</p>
-                <p className="mt-4 text-6xl font-bold text-black">$99</p>
-                <ul className="flex flex-col mt-8 space-y-1">
-                  <li className="text-base font-medium text-gray-900 lg:text-xl">
-                    Less than $2 a week
-                  </li>
-                  <li className="text-base font-medium text-gray-600 lg:text-xl">
-                    Billed every year
-                  </li>
-                  <li className="text-base font-medium text-gray-600 lg:text-xl">
-                    Cancel anytime
-                  </li>
-                </ul>
+                <p className="text-2xl font-bold  text-black">Other Services</p>
+                <div className="flex flex-col justify-start list list-disc mt-8 space-y-1 gap-y-4">
+                  <div className="text-base font-medium  text-gray-900 lg:text-xl flex gap-3 justify-center">
+                    <CircleCheck
+                      size={20}
+                      color="#000000"
+                      strokeWidth={3}
+                      absoluteStrokeWidth
+                      className="mt-1"
+                    />{" "}
+                    Less than $1 a day
+                  </div>
+                  <div className="text-base font-medium  text-gray-900 lg:text-xl flex gap-3 justify-center">
+                    <CircleCheck
+                      size={20}
+                      color="#000000"
+                      strokeWidth={3}
+                      absoluteStrokeWidth
+                      className="mt-1"
+                    />{" "}
+                    Less than $1 a day
+                  </div>
+                  <div className="text-base font-medium  text-gray-900 lg:text-xl flex gap-3 justify-center">
+                    <CircleCheck
+                      size={20}
+                      color="#000000"
+                      strokeWidth={3}
+                      absoluteStrokeWidth
+                      className="mt-1"
+                    />{" "}
+                    Less than $1 a day
+                  </div>
+                  <div className="text-base font-medium  text-gray-900 lg:text-xl flex gap-3 justify-center">
+                    <CircleCheck
+                      size={20}
+                      color="#000000"
+                      strokeWidth={3}
+                      absoluteStrokeWidth
+                      className="mt-1"
+                    />{" "}
+                    Less than $1 a day
+                  </div>
+                  <div className="text-base font-medium  text-gray-900 lg:text-xl flex gap-3 justify-center">
+                    <CircleCheck
+                      size={20}
+                      color="#000000"
+                      strokeWidth={3}
+                      absoluteStrokeWidth
+                      className="mt-1"
+                    />{" "}
+                    Less than $1 a day
+                  </div>
+                  <div className="text-base font-medium  text-gray-900 lg:text-xl flex gap-3 justify-center">
+                    <CircleCheck
+                      size={20}
+                      color="#000000"
+                      strokeWidth={3}
+                      absoluteStrokeWidth
+                      className="mt-1"
+                    />{" "}
+                    Less than $1 a day
+                  </div>
+                </div>
               </div>
             </div>
-          </div>
-
-          <div className="mt-8 text-center md:mt-16">
-            <a
-              href="#"
-              title=""
-              className="inline-flex items-center justify-center px-10 py-4 font-semibold text-white transition-all duration-200 rounded-md bg-gradient-to-r from-fuchsia-600 to-blue-600 focus:opacity-80 hover:opacity-80"
-              role="button"
-            >
-              {" "}
-              Create your account{" "}
-            </a>
-            <p className="mt-4 text-sm text-gray-700">
-              No Credit Card Required
-            </p>
           </div>
         </div>
       </section>
---
+++ src/components/Services/Workfolio.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Workfolio = () => {
+  return (
+    <>
+     workfolio 
+    </>
+  )
+}
+
+export default Workfolio
