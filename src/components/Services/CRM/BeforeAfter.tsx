71ecbb4963ebde9d182fc669c15c46afe8f83bb7
iam8uman <me8848you@gmail.com>
2024-04-02 17:41:31 +0545

    data section update

--- src/components/Services/CRM/BeforeAfter.tsx
+++ src/components/Services/CRM/BeforeAfter.tsx
@@ -1,17 +1,16 @@
 "use client";
 import React from "react";
-import {
-  ImageRevealCard,
-} from "@/components/ui/text-reveal-card";
+import { ImageRevealCard } from "@/components/ui/text-reveal-card";
 import BeforeImage from "@/../public/beforeImage.jpg";
 import AfterImage from "@/../public/afterimage.jpg";
 
 export default function BeforeAfter() {
   return (
     <div className="flex items-center justify-center  rounded-2xl w-full">
-      <ImageRevealCard beforeImage={BeforeImage.src} afterImage={AfterImage.src}>
-       
-      </ImageRevealCard>
+      <ImageRevealCard
+        beforeImage={BeforeImage.src}
+        afterImage={AfterImage.src}
+      ></ImageRevealCard>
     </div>
   );
 }
